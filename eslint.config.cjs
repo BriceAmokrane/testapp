f01ddcb1cc6fde253383254d652596d91fc586bb
BriceAmokrane <briceamokranedev@gmail.com>
2024-12-09 22:39:07 +0100

    ci/cd

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,51 +1,50 @@
-module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-    plugins: [
-        'react',
-        'react-hooks',
-        '@typescript-eslint',
-        'prettier'
-    ],
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    },
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                semi: false,
-                tabWidth: 4,
-                printWidth: 100,
-                trailingComma: 'es5'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                },
+                ecmaVersion: 'latest',
+                sourceType: 'module'
             }
-        ]
+        },
+        plugins: {
+            react: reactPlugin,
+            'react-hooks': reactHooksPlugin,
+            '@typescript-eslint': typescriptPlugin,
+            prettier: prettierPlugin
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    semi: false,
+                    tabWidth: 4,
+                    printWidth: 100,
+                    trailingComma: 'es5'
+                }
+            ]
+        }
     }
-}
+]
